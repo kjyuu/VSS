b0522d6f8a56eae36da5a41e555e8049763bc274
kjyu <jakwojtowicz@student.agh.edu.pl>
2023-11-05 21:33:38 +0100

    started adding the structure of the main program

--- vsapp_core/__main__.py
+++ vsapp_core/__main__.py
@@ -2,11 +2,13 @@ import dearpygui.dearpygui as dpg
 import numpy as np
 import cv2
 dpg.create_context()
-dpg.create_viewport(title='Vision System Studio', width=1100, height=1200)
+dpg.create_viewport(title='Vision System Studio', width=1920, height=1080)
+#dpg.configure_viewport("Vision System Studio",clear_color=[255,0,0,255],decorated=True)
 with dpg.font_registry():
         default_font=dpg.add_font("assets/fonts/quicksand/Quicksand_Bold.otf",18)
 dpg.bind_font(default_font)
-with dpg.window(tag="Primary Window"): 
+dpg.configure_app(docking=True, docking_space=False)
+with dpg.window(tag="Primary Window"): # main window of the program
     dpg.add_text("Welcome to Vision System Studio!")
     with dpg.menu_bar(): # menu bar
         with dpg.menu(label="Project"):
@@ -31,9 +33,24 @@ with dpg.window(tag="Primary Window"):
             dpg.add_menu_item(label="Show Style Editor", callback=lambda:dpg.show_tool(dpg.mvTool_Style))
             dpg.add_menu_item(label="Show Font Manager", callback=lambda:dpg.show_tool(dpg.mvTool_Font))
             dpg.add_menu_item(label="Show Item Registry", callback=lambda:dpg.show_tool(dpg.mvTool_ItemRegistry))
-    
+    with dpg.child_window(autosize_x=True, autosize_y=True,tag="child_window_main",no_scrollbar=True):
+        with dpg.table(tag="table",policy=dpg.mvTable_SizingStretchSame, header_row=False, borders_innerH=False,borders_outerH=False, borders_innerV=True, borders_outerV=False, resizable=True):
+                    dpg.add_table_column()
+                    dpg.add_table_column()
+                    dpg.add_table_column()
+                    with dpg.table_row():
+                        with dpg.child_window(autosize_y=True,width=dpg.get_item_width("Primary Window")//4) as mainColWind1:
+                            print(dpg.get_item_width("child_window_main"))
+                            print(dpg.get_item_width("child_window_main")//3)
+                        with dpg.child_window(autosize_y=True,width=dpg.get_item_width("Primary Window")//4) as mainColWind2:
+                            pass
+                        with dpg.child_window(autosize_y=True,width=dpg.get_item_width("Primary Window")//5) as mainColWind3:
+                            dpg.add_button(label="thirds")
+        #with dpg.group(horizontal=True) as mainCol1:
+           
 dpg.setup_dearpygui()
 dpg.set_primary_window("Primary Window",True)
 dpg.show_viewport()
-dpg.start_dearpygui()
+while dpg.is_dearpygui_running():
+    dpg.render_dearpygui_frame()
 dpg.destroy_context()
