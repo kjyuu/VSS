7b52306f3c76f6b75ef0687fdb651065ac95fee4
kjyu <jakwojtowicz@student.agh.edu.pl>
2023-12-14 23:25:51 +0100

    added dir selector and fonts

--- vsapp_core/__main__.py
+++ vsapp_core/__main__.py
@@ -3,7 +3,7 @@ import numpy as np
 import cv2
 import array
 dpg.create_context()
-dpg.create_viewport(title='Vision System Studio', width=1920, height=1080)
+dpg.create_viewport(title='Engineering Thesis App', width=1920, height=1080)
 #dpg.configure_viewport("Vision System Studio",clear_color=[255,0,0,255],decorated=True)
 
 def set_cam_param(sender,app_data,user_data):
@@ -17,7 +17,7 @@ ret,frame=cam1.read()
 frame_width=cam1.get(cv2.CAP_PROP_FRAME_WIDTH)
 frame_height=cam1.get(cv2.CAP_PROP_FRAME_HEIGHT)
 video_fps=cam1.get(cv2.CAP_PROP_FPS)
-print("values:",frame_width,frame_height,video_fps)
+print("W,H,FPS:",frame_width,frame_height,video_fps)
 cam_params = [
 cv2.CAP_PROP_FRAME_WIDTH,
 cv2.CAP_PROP_FRAME_HEIGHT,
@@ -36,23 +36,6 @@ cv2.CAP_PROP_ZOOM,
 cv2.CAP_PROP_FOCUS,
 cv2.CAP_PROP_BACKLIGHT,
 cv2.CAP_PROP_PAN]
-print(cam1.get(cv2.CAP_PROP_FRAME_WIDTH),
-cam1.get(cv2.CAP_PROP_FRAME_HEIGHT),
-cam1.get(cv2.CAP_PROP_FPS),
-cam1.get(cv2.CAP_PROP_CONTRAST),
-cam1.get(cv2.CAP_PROP_BRIGHTNESS),
-cam1.get(cv2.CAP_PROP_SATURATION),
-cam1.get(cv2.CAP_PROP_HUE),
-cam1.get(cv2.CAP_PROP_GAIN),
-cam1.get(cv2.CAP_PROP_EXPOSURE),
-cam1.get(cv2.CAP_PROP_SHARPNESS),
-cam1.get(cv2.CAP_PROP_GAMMA),
-cam1.get(cv2.CAP_PROP_TEMPERATURE),
-cam1.get(cv2.CAP_PROP_TRIGGER),
-cam1.get(cv2.CAP_PROP_ZOOM),
-cam1.get(cv2.CAP_PROP_FOCUS),
-cam1.get(cv2.CAP_PROP_BACKLIGHT),
-cam1.get(cv2.CAP_PROP_PAN))
 data=np.flip(frame,2)
 data=data.ravel()
 data=np.asfarray(data,dtype='f')
@@ -61,7 +44,12 @@ with dpg.texture_registry():
     dpg.add_raw_texture(frame_width,frame_height,texture_data_frame,format=dpg.mvFormat_Float_rgb,tag="texture_cam",label="tex_1")
 
 with dpg.font_registry():
-        default_font=dpg.add_font("assets/fonts/quicksand/Quicksand_Bold.otf",18)
+        dpg.add_font("assets/fonts/quicksand/Quicksand_Bold.otf",18)
+        dpg.add_font("assets/fonts/JetBrainsMono2.304/JetBrainsMono-Bold.ttf",18)
+        default_font=dpg.add_font("assets/fonts/JetBrainsMono2.304/JetBrainsMono-Medium.ttf",20)
+        dpg.add_font("assets/fonts/JetBrainsMono2.304/JetBrainsMono-Light.ttf",20)
+        dpg.add_font("assets/fonts/JetBrainsMono2.304/JetBrainsMono-Italic.ttf",20)
+        dpg.add_font("assets/fonts/JetBrainsMono2.304/JetBrainsMonoNL-Medium.ttf",20)
 dpg.bind_font(default_font)
 dpg.configure_app(docking=True, docking_space=False)
 with dpg.window(tag="Primary Window"): # main window of the program
@@ -96,47 +84,22 @@ with dpg.window(tag="Primary Window"): # main window of the program
                     dpg.add_table_column()
                     with dpg.table_row():
                         with dpg.child_window(autosize_y=True) as mainColWind1:
+                            with dpg.file_dialog(label="Demo File Dialog", width=800, height=400, show=False, callback=lambda s, a, u : print(s, a, u), tag="__demo_filedialog"):
+                                dpg.add_file_extension(".*", color=(255, 255, 255, 255))
+                                dpg.add_file_extension(".jpg", color=(0, 255, 0, 255))
+                                dpg.add_file_extension(".png", color=(0, 255, 0, 255))
+                                dpg.add_file_extension(".bmp", color=(0, 255, 0, 255))
+                                dpg.add_file_extension(".gif", color=(0, 255, 0, 255))
+                                #dpg.add_button(label="Button on file dialog")
+                            dpg.add_button(label="Show File Selector", user_data=dpg.last_container(), callback=lambda s, a, u: dpg.configure_item(u, show=True))
                             pass
                         with dpg.child_window(autosize_y=True) as mainColWind2:
                             for param in cam_params:
                                 dpg.add_input_int(label=param,default_value=cam1.get(param),callback=set_cam_param,user_data=param)
-                            # dpg.add_input_int(default_value=cam1.get(cv2.CAP_PROP_FRAME_WIDTH),callback=set_cam_param,user_data=cv2.CAP_PROP_FRAME_WIDTH)
-                            # dpg.add_input_int(default_value=cam1.get(cv2.CAP_PROP_FRAME_HEIGHT),callback=set_cam_param,user_data=cv2.CAP_PROP_FRAME_HEIGHT)
-                            # dpg.add_input_int(default_value=cam1.get(cv2.CAP_PROP_FPS),callback=set_cam_param,user_data=cv2.CAP_PROP_FPS)
-                            # dpg.add_input_int(default_value=cam1.get(cv2.CAP_PROP_CONTRAST),callback=set_cam_param,user_data=cv2.CAP_PROP_CONTRAST)
-                            # dpg.add_input_int(default_value=cam1.get(cv2.CAP_PROP_BRIGHTNESS),callback=set_cam_param,user_data=cv2.CAP_PROP_BRIGHTNESS)
-                            # dpg.add_input_int(default_value=cam1.get(cv2.CAP_PROP_SATURATION),callback=set_cam_param,user_data=cv2.CAP_PROP_SATURATION)
-                            # dpg.add_input_int(default_value=cam1.get(cv2.CAP_PROP_HUE),callback=set_cam_param,user_data=cv2.CAP_PROP_HUE)
-                            # dpg.add_input_int(default_value=cam1.get(cv2.CAP_PROP_GAIN),callback=set_cam_param,user_data=cv2.CAP_PROP_GAIN)
-                            # dpg.add_input_int(default_value=cam1.get(cv2.CAP_PROP_EXPOSURE),callback=set_cam_param,user_data=cv2.CAP_PROP_EXPOSURE)
-                            # dpg.add_input_int(default_value=cam1.get(cv2.CAP_PROP_SHARPNESS),callback=set_cam_param,user_data=cv2.CAP_PROP_SHARPNESS)
-                            # dpg.add_input_int(default_value=cam1.get(cv2.CAP_PROP_GAMMA),callback=set_cam_param,user_data=cv2.CAP_PROP_GAMMA)
-                            # dpg.add_input_int(default_value=cam1.get(cv2.CAP_PROP_TEMPERATURE),callback=set_cam_param,user_data=cv2.CAP_PROP_TEMPERATURE)
-                            # dpg.add_input_int(default_value=cam1.get(cv2.CAP_PROP_TRIGGER),callback=set_cam_param,user_data=cv2.CAP_PROP_TRIGGER)
-                            # dpg.add_input_int(default_value=cam1.get(cv2.CAP_PROP_ZOOM),callback=set_cam_param,user_data=cv2.CAP_PROP_ZOOM)
-                            # dpg.add_input_int(default_value=cam1.get(cv2.CAP_PROP_FOCUS),callback=set_cam_param,user_data=cv2.CAP_PROP_FOCUS)
-                            # dpg.add_input_int(default_value=cam1.get(cv2.CAP_PROP_BACKLIGHT),callback=set_cam_param,user_data=cv2.CAP_PROP_BACKLIGHT)
-                            # dpg.add_input_int(default_value=cam1.get(cv2.CAP_PROP_PAN),callback=set_cam_param,user_data=cv2.CAP_PROP_PAN)
-# cv2.CAP_PROP_FRAME_HEIGHT
-# cv2.CAP_PROP_FPS
-# cv2.CAP_PROP_CONTRAST
-# cv2.CAP_PROP_BRIGHTNESS
-# cv2.CAP_PROP_SATURATION
-# cv2.CAP_PROP_HUE
-# cv2.CAP_PROP_GAIN
-# cv2.CAP_PROP_EXPOSURE
-# cv2.CAP_PROP_SHARPNESS
-# cv2.CAP_PROP_GAMMA
-# cv2.CAP_PROP_TEMPERATURE
-# cv2.CAP_PROP_TRIGGER
-# cv2.CAP_PROP_ZOOM
-# cv2.CAP_PROP_FOCUS
-# cv2.CAP_PROP_BACKLIGHT
-# cv2.CAP_PROP_PAN    
+
                         with dpg.child_window(autosize_y=True) as mainColWind3:
                             dpg.add_button(label="thirds")
                             dpg.add_image("texture_cam")
-        #with dpg.group(horizontal=True) as mainCol1:
            
 dpg.setup_dearpygui()
 dpg.set_primary_window("Primary Window",True)
