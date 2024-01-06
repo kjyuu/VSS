cca6d67bb55848075b750b59f47acca1e529ac04
kjyu <jakwojtowicz@student.agh.edu.pl>
2024-01-06 01:22:10 +0100

    restructured main app, started distortion module

--- vsapp_core/__main__.py
+++ vsapp_core/__main__.py
@@ -1,52 +1,25 @@
 import dearpygui.dearpygui as dpg
-from filedialog20.fdialog import FileDialog
-
+from filedialog30.fdialog import FileDialog
+import app
 import numpy as np
 import cv2
 import array
 dpg.create_context()
 dpg.create_viewport(title='Engineering Thesis App', width=1600, height=900)
 dpg.set_viewport_always_top(True)
-#dpg.configure_viewport("Vision System Studio",clear_color=[255,0,0,255],decorated=True)
+selectedFilesDistortion=[]
+def outputSelectedForDistortion(selected_files):
+    outputwindow="txt_child"
+    #send selected_files outside the scope
+    global selectedFilesDistortion
+    selectedFilesDistortion=selected_files
+    #clear the window
+    dpg.delete_item(outputwindow, children_only=True)
+    for file in selected_files:
+        dpg.add_text(file, parent=outputwindow)
+        
+fd = FileDialog(callback=outputSelectedForDistortion, show_dir_size=False, modal=False, allow_drag=False,no_resize=False)
 
-def set_cam_param(sender,app_data,user_data):
-    print("sender is:",sender)
-    print("app_data is:",app_data)
-    print("user data is:",user_data)
-    cam1.set(user_data,app_data)
-
-# DISPLAYING A CAMERA IMAGE
-# cam1=cv2.VideoCapture(0)
-# ret,frame=cam1.read()
-# frame_width=cam1.get(cv2.CAP_PROP_FRAME_WIDTH)
-# frame_height=cam1.get(cv2.CAP_PROP_FRAME_HEIGHT)
-# video_fps=cam1.get(cv2.CAP_PROP_FPS)
-# print("W,H,FPS:",frame_width,frame_height,video_fps)
-# cam_params = [
-# cv2.CAP_PROP_FRAME_WIDTH,
-# cv2.CAP_PROP_FRAME_HEIGHT,
-# cv2.CAP_PROP_FPS,
-# cv2.CAP_PROP_CONTRAST,
-# cv2.CAP_PROP_BRIGHTNESS,
-# cv2.CAP_PROP_SATURATION,
-# cv2.CAP_PROP_HUE,
-# cv2.CAP_PROP_GAIN,
-# cv2.CAP_PROP_EXPOSURE,
-# cv2.CAP_PROP_SHARPNESS,
-# cv2.CAP_PROP_GAMMA,
-# cv2.CAP_PROP_TEMPERATURE,
-# cv2.CAP_PROP_TRIGGER,
-# cv2.CAP_PROP_ZOOM,
-# cv2.CAP_PROP_FOCUS,
-# cv2.CAP_PROP_BACKLIGHT,
-# cv2.CAP_PROP_PAN]
-# data=np.flip(frame,2)
-# data=data.ravel()
-# data=np.asfarray(data,dtype='f')
-# texture_data_frame=np.true_divide(data,255.0)
-# with dpg.texture_registry():
-#    dpg.add_raw_texture(frame_width,frame_height,texture_data_frame,format=dpg.mvFormat_Float_rgb,tag="texture_cam",label="tex_1")
-    
 with dpg.font_registry():
         dpg.add_font("assets/fonts/quicksand/Quicksand_Bold.otf",18)
         dpg.add_font("assets/fonts/JetBrainsMono2.304/JetBrainsMono-Bold.ttf",18)
@@ -57,7 +30,6 @@ with dpg.font_registry():
 dpg.bind_font(default_font)
 dpg.configure_app(docking=True, docking_space=False)
 with dpg.window(tag="Primary Window"): # main window of the program
-    dpg.add_text("Welcome to the App!")
     with dpg.menu_bar(): # menu bar
         with dpg.menu(label="Project"):
             dpg.add_menu_item(label="New")
@@ -81,46 +53,23 @@ with dpg.window(tag="Primary Window"): # main window of the program
             dpg.add_menu_item(label="Show Style Editor", callback=lambda:dpg.show_tool(dpg.mvTool_Style))
             dpg.add_menu_item(label="Show Font Manager", callback=lambda:dpg.show_tool(dpg.mvTool_Font))
             dpg.add_menu_item(label="Show Item Registry", callback=lambda:dpg.show_tool(dpg.mvTool_ItemRegistry))
-    with dpg.child_window(autosize_x=True, autosize_y=True,tag="child_window_main",no_scrollbar=True):
-        with dpg.table(tag="table",policy=dpg.mvTable_SizingStretchSame, header_row=False, borders_innerH=False,borders_outerH=False, borders_innerV=True, borders_outerV=False, resizable=True):
-                    dpg.add_table_column()
-                    dpg.add_table_column()
-                    dpg.add_table_column()
-                    with dpg.table_row():
-                        with dpg.child_window(autosize_y=True) as mainColWind1:
-                            with dpg.file_dialog(label="Demo File Dialog", width=800, height=400, show=False, callback=lambda s, a, u : print(s, a, u), tag="__demo_filedialog"):
-                                dpg.add_file_extension(".*", color=(255, 255, 255, 255))
-                                #dpg.add_file_extension(".jpg", color=(0, 255, 0, 255))
-                                #dpg.add_file_extension(".png", color=(0, 255, 0, 255))
-                                #dpg.add_file_extension(".bmp", color=(0, 255, 0, 255))
-                                #dpg.add_file_extension(".gif", color=(0, 255, 0, 255))
-                                #dpg.add_button(label="Button on file dialog")
-                            dpg.add_button(label="Show File Selector", user_data=dpg.last_container(), callback=lambda s, a, u:  dpg.configure_item(u, show=True))
-                            pass
-                        with dpg.child_window(autosize_y=True) as mainColWind2:
-                            #for param in cam_params:
-                            #    dpg.add_input_int(label=param,default_value=cam1.get(param),callback=set_cam_param,user_data=param)
-                            dpg.add_input_text(label="Text Input 1", source="string_value")
-                            dpg.add_button(label="button load image")
-                            width, height, channels, data = dpg.load_image("assets/images/o1z01.bmp")
-                            with dpg.texture_registry(show=True):
-                                dpg.add_static_texture(width=width, height=height, default_value=data, tag="tex41")
-                            dpg.add_image("tex41")
-                        with dpg.child_window(autosize_y=True) as mainColWind3:
-                            dpg.add_button(label="thirds")
-                            # dpg.add_image("texture_cam")
-           
+    #with dpg.child_window(autosize_x=True, autosize_y=True,tag="child_window_main",no_scrollbar=True):
+    with dpg.table(tag="table",policy=dpg.mvTable_SizingStretchSame, header_row=False, borders_innerH=False,borders_outerH=False, borders_innerV=True, borders_outerV=False, resizable=True) as guiTableMain:
+                dpg.add_table_column(init_width_or_weight=0.4)
+                dpg.add_table_column()
+                with dpg.table_row():
+                    with dpg.child_window(autosize_y=True) as mainColWind1: 
+                        dpg.add_text("Image Distortion Module")
+                        dpg.add_button(label="Select files", callback=fd.show_file_dialog)
+                        dpg.add_child_window(width=-1, height=200, tag="txt_child",horizontal_scrollbar=True)
+                        dpg.add_button(label="Start", callback=test)#app.startDistortion(selectedFilesDistortion))
+                    with dpg.child_window(autosize_y=True) as mainColWind2:
+                        dpg.add_input_text(label="Text Input 1", source="string_value")
+                    test()
 dpg.setup_dearpygui()
 dpg.set_primary_window("Primary Window",True)
 dpg.show_viewport()
 dpg.set_viewport_always_top(False)
 while dpg.is_dearpygui_running():
-    # ret,frame=cam1.read()
-    # data=np.flip(frame,2)
-    # data=data.ravel()
-    # data=np.asfarray(data,dtype='f')
-    # texture_data_frame=np.true_divide(data,255.0)
-    # dpg.set_value("texture_cam",texture_data_frame)
     dpg.render_dearpygui_frame()
-# cam1.release()
 dpg.destroy_context()
--- vsapp_core/app.py
+++ vsapp_core/app.py
@@ -1,3 +1,6 @@
+import os
+import numpy as np
+import cv2
 def create_new_project():
     pass
 def open_project():
@@ -18,3 +21,34 @@ def open_project_view():
     pass
 def close_project_view():
     pass
+def startDistortion(selectedFiles):
+    print(np.size(selectedFiles))
+    for file in selectedFiles:
+        #check if file is image type
+        if os.path.splitext(file)[1] in ['.jpg', '.png', '.jpeg', '.bmp','.tif']:
+            print(file, "is an image file")
+            true_dst=cv2.imread(file)
+            src = cv2.imread("img2.png")
+
+            # ground truth homography from true_dst to src
+            H = np.array([
+                [8.7976964e-01,   3.1245438e-01,  -3.9430589e+01],
+                [-1.8389418e-01,   9.3847198e-01,   1.5315784e+02],
+                [1.9641425e-04,  -1.6015275e-05,   1.0000000e+00]])
+
+            # create indices of the destination image and linearize them
+            h, w = true_dst.shape[:2]
+            indy, indx = np.indices((h, w), dtype=np.float32)
+            lin_homg_ind = np.array([indx.ravel(), indy.ravel(), np.ones_like(indx).ravel()])
+
+            # warp the coordinates of src to those of true_dst
+            map_ind = H.dot(lin_homg_ind)
+            map_x, map_y = map_ind[:-1]/map_ind[-1]  # ensure homogeneity
+            map_x = map_x.reshape(h, w).astype(np.float32)
+            map_y = map_y.reshape(h, w).astype(np.float32)
+
+            # remap!
+            dst = cv2.remap(src, map_x, map_y, cv2.INTER_LINEAR)
+            blended = cv2.addWeighted(true_dst, 0.5, dst, 0.5, 0)
+            cv2.imshow('blended.png', blended)
+            cv2.waitKey()
